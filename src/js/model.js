039dc2af48b9281a90c928d7a63b6d01abd5862d
ElGutiar <el12guitar@gmail.com>
2020-12-27 12:05:19 +0300

    MVC architecture

---
+++ src/js/model.js
@@ -0,0 +1,28 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}/${id}`);
+
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      image: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+      cookingTime: recipe.cooking_time,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    console.error(`${err} 💥`);
+  }
+};
