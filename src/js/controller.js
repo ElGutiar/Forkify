8d2a587f704dbf709bd6ab0760fc24601af87df5
ElGutiar <el12guitar@gmail.com>
2021-01-03 14:37:17 +0300

    Added a new features

--- src/js/controller.js
+++ src/js/controller.js
@@ -122,6 +122,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeatures = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBoomarks);
   recipeView.addHandlerRender(controlRecipe);
@@ -130,5 +134,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
+  newFeatures();
 };
 init();
